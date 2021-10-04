61b102682a7e298233234e7568d1205060efa032
Legier <Legier@LAPTOP-09FN9Q5K>
2021-10-04 14:32:23 +0200

    creation dossier

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,39 @@
+/* 
+* TP 0 EXO 1
+* MRG
+*Noa LEGIER 
+*27 septembre 2021
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Legier
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("Bonjour");
+       
+       String prenom ;
+       Scanner sc;
+       sc = new Scanner(System.in);
+       System.out.println("Bonjour, quel est votre prenom ? (; ");
+       prenom = sc.nextLine();
+       
+       System.out.println("Aurevoir");
+ 
+    }
+    
+}
